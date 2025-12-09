cbf9943dd2619b2b340910814fa23269acc2a6af
kokom <ka.mahrous@gmail.com>
2025-12-09 12:59:26 +0200

    Workflow Commit

--- src/test/java/AddTest.java
+++ src/test/java/AddTest.java
@@ -1,9 +1,7 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class tests {
-    @Test
+public class AddTest {
+    @org.junit.jupiter.api.Test
     void testAdd() {
         assertEquals(5, Backend.add(2, 3), "2 + 3 should equal 5");
     }
