7e0e085e2d067f275734791e607d2a3f2a5a56bf
kokom <ka.mahrous@gmail.com>
2025-12-10 19:20:39 +0200

    Workflow Commit

--- src/main/java/Backend.java
+++ src/main/java/Backend.java
@@ -1,6 +1,6 @@
 public class Backend {
     public static int add(int a, int b) {
-        int result = a-b;
+        int result = a+b;
         return result;
     }
     public static void main(String[] args) {
